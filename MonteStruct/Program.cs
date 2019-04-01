ee6db432f4c599ff0075b915f01a992abee2e3f8
Doug Brauch <doug.c.brauch@outlook.com>
2019-04-01 13:20:03 -0400

    Calculates hypotenuse and returns # of points within circle and outside of the circle.

--- MonteStruct/Program.cs
+++ MonteStruct/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace MonteStruct
 {
     public struct Coords
@@ -16,9 +15,9 @@ namespace MonteStruct
             y = p2;
         }
 
-       double Hypotenuse()
+       public double Hypotenuse()
         {
-            double hypoteneuse = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
         }
     }
 
@@ -33,8 +32,10 @@ namespace MonteStruct
             double s = double.Parse(Console.ReadLine());
             int s1 = (int)s;
 
-            double x;
-            double y;
+            //double x;
+            //double y;
+            int inCircle = 0;
+            int outCircle = 0;
 
             Coords[] point = new Coords[s1];
 
@@ -46,26 +47,32 @@ namespace MonteStruct
 
             foreach (Coords i in point)
             {
-                Console.WriteLine("\tx = {0:f3}, y = {1:f3}", i.x, i.y);
+                //Console.WriteLine("\tx = {0:f3}, y = {1:f3}", i.x, i.y);
 
-                //    if (i.Hypotenuse() <= 1.0)
-                //    {
-                //        ++inCircle;
-                //    }
-                //}
-
-                //    each (var i in point)
-                //{
-                //    Console.WriteLine;
-                //}
-                //Console.ReadLine();
-
-                //z = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
-
-                //Console.WriteLine(Math.Round(z, 3));
-
-                //
+                if (i.Hypotenuse() <= 1.0)
+                {
+                    ++inCircle;
+                }
+                else if(i.Hypotenuse() > 1.0)
+                {
+                    ++outCircle;
+                }
             }
+                Console.WriteLine(outCircle);
+                Console.WriteLine(inCircle);             
+            Console.ReadLine();
+            //    each (var i in point)
+            //{
+            //    Console.WriteLine;
+            //}
+            //Console.ReadLine();
+
+            //z = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+
+            //Console.WriteLine(Math.Round(z, 3));
+
+            //
+        
         } 
     }
 }
